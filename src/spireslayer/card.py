7aa3839d8a30091e8e4b3dcc934f1383dbd88e45
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2026-03-11 17:19:17 +0700

    move defect card from ironclad_card.py to Card

--- src/spireslayer/card.py
+++ src/spireslayer/card.py
@@ -180,6 +180,24 @@ class Card(object):
         UNLOAD = "Unload"
         WRAITH_FORM = "Wraith Form"
 
+    class Defect:
+        STRIKE = "Strike_B"
+        DEFEND = "Defend_B"
+
+        LEAP = "Leap"
+        DUALCAST = "Dualcast"
+        DEFRAGMENT = "Defragment"
+        CAPACITOR = "Capacitor"
+
+        # lightning
+        ZAP = "Zap"
+        THUNDER_STRIKE = "Thunder Strike"
+
+        # ice
+        COLD_SNAP = "Cold Snap"
+        BLIZZARD = "Blizzard"
+        GLACIER = "Glacier"
+
     class Watcher:
         # Starter Cards
         DEFEND_W = "Defend"
