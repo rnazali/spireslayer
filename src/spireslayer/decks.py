ae1736114de9ef88b91ef7f0556454d103259656
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2026-03-11 17:23:59 +0700

    move ironclad deck to decks.py

--- src/spireslayer/decks.py
+++ src/spireslayer/decks.py
@@ -23,6 +23,42 @@ class Deck(object):
 
 class ExampleDeck:
     @staticmethod
+    def ironclad_strength():
+        return Deck([
+            Card(Card.Ironclad.FLEX),
+            Card(Card.Ironclad.FLEX),
+            Card(Card.Ironclad.INFLAME),
+            Card(Card.Ironclad.INFLAME),
+            Card(Card.Ironclad.RUPTURE),
+            Card(Card.Ironclad.SPOT_WEAKNESS),
+            Card(Card.Ironclad.LIMIT_BREAK),
+            Card(Card.Ironclad.LIMIT_BREAK),
+            Card(Card.Ironclad.HEAVY_BLADE),
+            Card(Card.Ironclad.HEAVY_BLADE),
+            Card(Card.Ironclad.HEAVY_BLADE),
+            Card(Card.Ironclad.HEAVY_BLADE),
+        ])
+
+    @staticmethod
+    def ironclad_block():
+        return Deck([
+            Card(Card.Ironclad.BARRICADE),
+            Card(Card.Ironclad.ENTRENCH),
+            Card(Card.Ironclad.BODY_SLAM),
+            Card(Card.Ironclad.BODY_SLAM),
+            Card(Card.Ironclad.BODY_SLAM),
+            Card(Card.Ironclad.BODY_SLAM),
+            Card(Card.Ironclad.BODY_SLAM),
+            Card(Card.Ironclad.SHRUG_IT_OFF),
+            Card(Card.Ironclad.SHRUG_IT_OFF),
+            Card(Card.Ironclad.SHRUG_IT_OFF),
+            Card(Card.Ironclad.IMPERVIOUS),
+            Card(Card.Ironclad.IMPERVIOUS),
+            Card(Card.Ironclad.IMPERVIOUS),
+        ])
+
+
+    @staticmethod
     def defect_lightning():
         return Deck([
             Card(Card.Defect.LEAP),
