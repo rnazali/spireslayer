24f5be7cc441aba284b254c1b3330f1e39a7d310
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2026-03-12 09:42:21 +0700

    fix os-related path joining on test

--- source/tests/test_editor.py
+++ source/tests/test_editor.py
@@ -1,17 +1,21 @@
+import os
+
 from spireslayer.card import Card
 from spireslayer.deck import Deck
 from spireslayer.editor import Editor
 
+autosave_path = os.path.join("tests", "test.autosave")
+
 
 def test_initialization():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_save_file = editor.json
     assert len(original_save_file) == 112
     assert editor.encryption_key == "key"
 
 
 def test_set_json():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_save_file = editor.json
     new_save_file = {}
     editor.json = new_save_file
@@ -21,7 +25,7 @@ def test_set_json():
 
 
 def test_update_current_health():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_health = editor.json['current_health']
     assert original_health == 500
     editor.update_current_health(100)
@@ -31,7 +35,7 @@ def test_update_current_health():
 
 
 def test_update_max_health():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_max_health = editor.json['max_health']
     assert original_max_health == 500
     editor.update_max_health(100)
@@ -41,7 +45,7 @@ def test_update_max_health():
 
 
 def test_update_max_orbs():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_max_orbs = editor.json['max_orbs']
     assert original_max_orbs == 10
     editor.update_max_orbs(15)
@@ -51,7 +55,7 @@ def test_update_max_orbs():
 
 
 def test_update_hand_size():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_hand_size = editor.json['hand_size']
     assert original_hand_size == 10
     editor.update_hand_size(15)
@@ -61,7 +65,7 @@ def test_update_hand_size():
 
 
 def test_update_energy_per_turn():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     original_energy_per_turn = editor.json['red']
     assert original_energy_per_turn == 20
     editor.update_energy_per_turn(30)
@@ -71,7 +75,7 @@ def test_update_energy_per_turn():
 
 
 def test_set_deck():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     deck = Deck([
         Card("1"),
         Card("2"),
@@ -83,7 +87,7 @@ def test_set_deck():
 
 
 def test_add_card():
-    editor = Editor(autosave_path=r"tests\test.autosave")
+    editor = Editor(autosave_path=autosave_path)
     deck = Deck([
         Card("1"),
         Card("2"),
